eda3f57aa6c3bb53bc141610b028f325a14f3b34
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-04-24 19:12:10 +0900

    Your First API

---
+++ todo-api/project.clj
@@ -0,0 +1,11 @@
+(defproject todo-api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[io.pedestal/pedestal.jetty "0.5.3"]
+                 [io.pedestal/pedestal.route "0.5.3"]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.slf4j/slf4j-simple "1.7.25"]])
